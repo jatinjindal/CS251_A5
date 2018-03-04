cb9410b9b0f43b9f6322738b8a5d312b79d79871
jatinjindal <jatinjindal369@gmail.com>
2018-03-04 21:02:16 +0530

    modified hello.c

--- hello.c
+++ hello.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-void microkernel_sendmsg(char *);
 void main(){
 printf("Helloworld!\n");
 printf("This must be a monolithic
 design\n");
 microkernel_sendmsg("is more
-portable");
-}
+portable");}
 void microkernel_sendmsg(char *a){
 printf("microkernel: %s\n", a);
 }
